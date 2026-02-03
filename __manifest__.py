38e9760a6c430436a20ac8e5c475c12e29f17f97
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-02-03 12:45:55 +0100

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'migration_test',
+    'version': '19.0.0.1.0',
+    'author': 'Emanuele',
+    'license': 'LGPL-3',
+    'depends': ['base', 'crm'], 
+    'description':'testing migrations and other...',
+    'demo':[],
+    'depends':['hr'],
+    'installable': True,
+}
